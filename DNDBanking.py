91a77fe67ecfeaa2e843a2bd5d2e9cd0c382fd9f
AWCampbell <130492182+ShadySheep350@users.noreply.github.com>
2026-07-21 16:05:01 -0400

    Just uploading the .py for now
    as its not a large project yet, imma just drop the file in manually

---
+++ DNDBanking.py
@@ -0,0 +1,13 @@
+def main():
+    print("This is Bait's banking app")
+    #  features:
+    #  saved data
+    #  gold converter
+    #  formatted recipt .txt file writer
+    # sort or search purchases
+def homemenu():
+    choice = input("Please make a selection: ")
+    
+
+if __name__=="__main__":
+        main()
